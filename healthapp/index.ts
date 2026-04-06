f5bf05345ae2038486bd8afe106fb61d855b9b24
teki96 <teemu.t.kinnunen@tuni.fi>
2026-04-06 23:01:04 +0300

    fix linting

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,39 +1,39 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+const app = express();
 
-import { calculateBmi } from './bmiCalculator.ts'
-import { calculateExercises } from './exerciseCalculator.ts'
+import { calculateBmi } from './bmiCalculator.ts';
+import { calculateExercises } from './exerciseCalculator.ts';
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!')
-})
+  res.send('Hello Full Stack!');
+});
 
 app.get('/bmi', (req, res) => {
-  const { height, weight } = req.query
+  const { height, weight } = req.query;
 
   if (!height || !weight) {
-    return res.status(400).json({ error: 'malformatted parameters' })
+    return res.status(400).json({ error: 'malformatted parameters' });
   }
 
   if (isNaN(Number(height)) || isNaN(Number(weight))) {
-    return res.status(400).json({ error: 'malformatted parameters' })
+    return res.status(400).json({ error: 'malformatted parameters' });
   }
 
-  const bmi = calculateBmi(Number(height), Number(weight))
+  const bmi = calculateBmi(Number(height), Number(weight));
 
   return res.json({
     weight: Number(weight),
     height: Number(height),
     bmi: bmi,
-  })
-})
+  });
+});
 
 app.post('/exercises', express.json(), (req, res) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { daily_exercises, target } = req.body
+  const { daily_exercises, target } = req.body;
 
   if (!daily_exercises || !target) {
-    return res.status(400).json({ error: 'parameters missing' })
+    return res.status(400).json({ error: 'parameters missing' });
   }
 
   if (
@@ -41,16 +41,16 @@ app.post('/exercises', express.json(), (req, res) => {
     isNaN(Number(target)) ||
     daily_exercises.some((day) => isNaN(Number(day)))
   ) {
-    return res.status(400).json({ error: 'malformatted parameters' })
+    return res.status(400).json({ error: 'malformatted parameters' });
   }
 
-  const result = calculateExercises(daily_exercises.map(Number), Number(target))
+  const result = calculateExercises(daily_exercises.map(Number), Number(target));
 
-  return res.json(result)
-})
+  return res.json(result);
+});
 
-const PORT = 3000
+const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
